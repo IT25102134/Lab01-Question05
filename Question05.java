7cc22d7683ecfe708c2b94f4f378eef0b4250d01
IT25102134 <it25102134@my.sliit.lk>
2026-02-06 15:58:03 +0530

    Update Question05.java

--- Question05.java
+++ Question05.java
@@ -1,17 +1,14 @@
 
 class Calculator {
 
-    
-    int add(int a, int b) {
+      int add(int a, int b) {
         return a + b;
     }
 
-   
     int multiply(int a, int b) {
         return a * b;
     }
 
-    
     int square(int a) {
         return a * a;
     }
@@ -19,6 +16,7 @@ class Calculator {
 
 
 public class Question05{
+    
     public static void main(String [] args) {
 
         Calculator c = new Calculator();
@@ -27,6 +25,7 @@ public class Question05{
         int x1 = c.multiply(3, 4);
         int x2 = c.multiply(5, 7);
         int x3 = c.add(x1, x2);
+        
         int result1 = c.square(x3);
 
         System.out.println("(3*4 + 5*7)^2 = " + result1);
@@ -34,7 +33,6 @@ public class Question05{
         // (4 + 7)^2 + (8 + 3)^2
         int y1 = c.add(4, 7);
         int y2 = c.square(y1);
-
         int y3 = c.add(8, 3);
         int y4 = c.square(y3);
 
